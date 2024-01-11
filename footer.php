3ab6dfd474763b14cb9d67748dbcb1b1cfba212b
buddhikaHaz <156274873+buddhikaHaz@users.noreply.github.com>
2024-01-11 09:36:38 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ */
+?>
+		</div><!-- #content -->
+<?php $language = isset($_GET['lang']) ? $_GET['lang'] : '';
+global $hideFooter; if (!$hideFooter) {
+$translations = '';
+if ($language) {
+    if (file_exists(get_stylesheet_directory() . '/' . 'template-parts/' . '/translations/' . $language .'/footer-content' . '.php')) {
+        $translations = '/translations/' . $language;
+    }
+}
+ob_start();
+get_template_part('template-parts' . $translations . '/footer-content'); ?>
+        
+<?php } ?>
+        
+        
+	</div><!-- .site-inner -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<?php back_to_top(); ?>
+</body>
+</html>
+<?php $footer = ob_get_clean();
+if (function_exists('renderTemplate')) {
+    renderTemplate($footer, '', 'echo', 'footer');
+} else {
+    echo $footer;
+} ?>
